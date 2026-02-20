2c317ed89b76fe40a2a8472fc2c001621f8ac44b
João Pedro <jpbentosperandio@gmail.com>
2026-02-20 10:52:02 -0300

    commit update CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { ForbiddenException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.useGlobalPipes(
+
+  // Adicionei isso pra ver se funciona o frontend --> Tava dando erro de CORS
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  // 
+
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
