75fdaa57f1c2fc168b810d1a258689f6d818704e
Cesar <cesaraugusto23121989@gmail.com>
2026-03-04 01:49:13 -0300

    feat: add queue to rooms module

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -6,9 +6,11 @@ import { RoomsService } from './service/rooms.service'
 
 import { Room } from './model/room.model'
 import { RoomPlayer } from './model/room-player.model'
+import { BullModule } from '@nestjs/bull'
 
 @Module({
     imports: [
+        BullModule.registerQueue({name: "queue-teste"}),
         TypeOrmModule.forFeature([Room, RoomPlayer]),
     ],
     controllers: [RoomsController],
