7ca220ec8f9967ba010fbb0ae0334503b4be4d73
Cesar <cesaraugusto23121989@gmail.com>
2026-03-06 13:48:25 -0300

    fix: fix merge bug

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { BullModule } from '@nestjs/bull'
+ import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bull';
 
-import { RoomsController } from './controller/rooms.controller'
-import { RoomsService } from './service/rooms.service'
+import { RoomsController } from './controller/rooms.controller';
+import { RoomsService } from './service/rooms.service';
 
-import { Room } from './model/room.model'
-import { RoomPlayer } from './model/room-player.model'
-<<<<<<< HEAD
+import { Room } from './model/room.model';
+import { RoomPlayer } from './model/room-player.model';
+
+import { RoomPlayerRepositoryCustom } from './repository/room.player.repository';
 
 @Module({
   imports: [
@@ -15,20 +16,13 @@ import { RoomPlayer } from './model/room-player.model'
     TypeOrmModule.forFeature([Room, RoomPlayer]),
   ],
   controllers: [RoomsController],
-  providers: [RoomsService],
-  exports: [RoomsService],
-=======
-import { BullModule } from '@nestjs/bull'
-import { RoomPlayerRepositoryCustom } from './repository/room.player.repository'
-
-@Module({
-    imports: [
-        BullModule.registerQueue({name: "queue-teste"}),
-        TypeOrmModule.forFeature([Room, RoomPlayer]),
-    ],
-    controllers: [RoomsController],
-    providers: [RoomsService, RoomPlayerRepositoryCustom],
-    exports: [RoomsService, RoomPlayerRepositoryCustom],
->>>>>>> cde3a63 (fix: export repository Room Player)
+  providers: [
+    RoomsService, 
+    RoomPlayerRepositoryCustom 
+  ],
+  exports: [
+    RoomsService, 
+    RoomPlayerRepositoryCustom 
+  ],
 })
 export class RoomsModule {}
